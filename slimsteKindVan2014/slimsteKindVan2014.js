6d41dcb50fa3ff64719b38f79c0715ecb68d7b7a
Kristof Buts <kristof.buts@gmail.com>
2015-01-05 20:11:11 +0100

    js file slimsteKindVan2014 toegevoegd

---
+++ slimsteKindVan2014/slimsteKindVan2014.js
@@ -0,0 +1,44 @@
+var dataHetSlimsteKindVan2014 = {
+	titel: 'Het slimste kind van 2014',
+	drieZesNegen: {
+		links: {
+		}
+	},
+	galerijen: [
+			{baseUrl:'slimsteKindVan2014/galerij/studio100/',
+			 fotos: [{url: 'mega-mindy.jpg'}, {url: 'plop.jpg'}, {url: 'dobus.jpg'},
+			 		 {url: 'piet_piraat.jpg'}, {url: 'wickie.jpg'}, {url: 'samson.jpg'},
+			 		 {url: 'lui.jpg'}, {url: 'bobo.jpg'}, {url: 'maya.jpg'},
+			 		 {url: 'rox.jpg'} ]},
+			{baseUrl:'slimsteKindVan2014/galerij/disney/',
+			 fotos: [{url: 'knabbel_en_babbel.jpg'}, {url: 'mickey.jpg'}, {url: 'sneeuwwitje.jpg'},
+			 		 {url: 'winnie.jpg'}, {url: 'bambi.jpg'}, {url: 'tijgetje.jpg'},
+			 		 {url: 'donald_duck.jpg'}, {url: 'kleine_zeemeermin.jpg'}, {url: 'dumbo.gif'},
+			 		 {url: 'pluto.jpg'} ]},
+			{baseUrl:'slimsteKindVan2014/galerij/dieren/',
+			 fotos: [{url: 'arend.jpg'}, {url: 'beer.jpg'}, {url: 'chimpansee.jpg'},
+			 		 {url: 'giraf.jpg'}, {url: 'leeuw.jpg'}, {url: 'neushoorn.jpg'},
+			 		 {url: 'olifant.jpg'}, {url: 'tijger.jpg'}, {url: 'uil.jpg'},
+			 		 {url: 'zebra.jpg'} ]}
+			],
+	opendeur: {
+		vragen: [
+		{urlFoto:'slimsteKindVan2014/opendeur/todo.jpg', urlFilm: 'slimsteKindVan2014/opendeur/todo.mp4', 
+		 antwoorden: ['TODO', 'TODO', 'TODO', 'TODO'],
+		 link: 'slimsteKindVan2014/opendeur/something.jpg'},
+		{urlFoto:'slimsteKindVan2014/opendeur/todo.jpg', urlFilm: 'slimsteKindVan2014/opendeur/todo.mp4', 
+		 antwoorden: ['TODO', 'TODO', 'TODO', 'TODO']},
+		{naam: 'Omschrijving 3', urlFoto:'slimsteKindVan2014/opendeur/todo.jpg', urlFilm: 'slimsteKindVan2014/opendeur/todo.mp4', 
+		 antwoorden: ['TODO', 'TODO', 'TODO', 'TODO']}
+		 ]
+	},
+	puzzels: [
+	],
+	collectiefGeheugen: {
+		videos: [],
+	},
+	finale: [
+		{ vraag: 'TODO',
+		  antwoorden: ['TODO', 'TODO', 'TODO', 'TODO', 'TODO'] }
+	]
+};
