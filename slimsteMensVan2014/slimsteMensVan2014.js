c14a6c28226fafa387d7b489f9b6d06f51f3c41f
Kristof Buts <kristof.buts@gmail.com>
2015-01-05 20:12:22 +0100

    js file slimsteMensVan2014 toegevoegd

---
+++ slimsteMensVan2014/slimsteMensVan2014.js
@@ -0,0 +1,131 @@
+var dataDeSlimsteMensVan2014Data = {
+	titel: 'De slimste mens van 2014',
+	drieZesNegen: {
+		links: {
+			vraag1:  'slimsteMensVan2014/driezesnegen/opgave_1.html',
+			vraag6:  'slimsteMensVan2014/driezesnegen/filmpje_3-6-9.mp4',
+			vraag8:  'slimsteMensVan2014/driezesnegen/Kerstboom.png',
+			vraag11:  'slimsteMensVan2014/driezesnegen/Song_3-6-9.mp3',
+			vraag15: 'slimsteMensVan2014/driezesnegen/opgave_15.html'
+		}
+	},
+	opendeur: {
+		vragen: [
+		{urlFoto:'slimsteMensVan2014/opendeur/stefan.jpg', urlFilm: 'slimsteMensVan2014/opendeur/stefan.mp4', 
+		 antwoorden: ['Remake', 'Erik Van Looy', 'Bart de Pauw', 'Matthias Schoenaerts'] },
+		{urlFoto:'slimsteMensVan2014/opendeur/Hanne_en_Yara.jpg', urlFilm: 'slimsteMensVan2014/opendeur/Hanne_en_Yara.avi', 
+		 antwoorden: ['Judoka', 'Comeback', 'Cocaïne', 'Geschorst'],
+		 link: 'slimsteMensVan2014/opendeur/Mr_Bean-Judo_Class.mp4' },
+		{urlFoto:'slimsteMensVan2014/opendeur/tim.jpg', urlFilm: 'slimsteMensVan2014/opendeur/tim.mp4', 
+		 antwoorden: ['Zanger', 'Gorki', 'Mia', 'Schrijver']}
+		 ]
+	},
+	puzzels: [
+		{ antwoorden: [
+			{antwoord: 'Jack Bauer', hints: ['24 uren', 'Geen broer van Bob', 'CTU', 'De wereld redden']},
+			{antwoord: 'Philippe Geubels', hints: ['Colruyt', 'Geen broer van Joseph', 'Komiek', 'Kaal']},
+			{antwoord: 'Pharrell Williams', hints: ['Geen broer van Venus', 'Geen broer van Serena', 'Geen broer van Robbie', 'Happy']}
+		]},
+		{ antwoorden: [
+			{antwoord: 'Electrabel', hints: ['het', 'is', 'jouw', 'energie']},
+			{antwoord: 'Jetair', hints: ['de mooiste', 'tijd', 'van', 'het jaar']},
+			{antwoord: 'Mediamarkt', hints: ['ik', 'ben', 'toch', 'niet gek']}
+		]},
+		{ antwoorden: [
+			{antwoord: 'Take me to church', hints: ['I was born sick', 'But I love it', 'Command me to be well', 'Amen (4x) ...']},
+			{antwoord: 'Sexy als ik dans', hints: ['Ze zegt:', 'weet je wat het is', 'baby', 'Ik voel me ...']},
+			{antwoord: 'Home', hints: ['Through the warmth,', 'through the cold,', 'keep running till we\'re there.', 'We\'re coming ... now']}
+		]}
+	],
+	galerijen: [
+			{baseUrl:'slimsteMensVan2014/galerij/galerij1/',
+			 fotos: [{url: '10.jpg'}, {url: '11.jpg'}, {url: '12.jpg'},
+			 		 {url: '13.jpg'}, {url: '14.jpg'}, {url: '15.jpg'},
+			 		 {url: '16.jpg'}, {url: '17.jpg'}, {url: '18.jpg'},
+			 		 {url: '19.jpg'} ] },
+			{baseUrl:'slimsteMensVan2014/galerij/galerij2/',
+			 fotos: [{url: '20.jpg'}, {url: '21.jpg'}, {url: '22.jpg'},
+			 		 {url: '23.jpg'}, {url: '24.jpg'}, {url: '25.jpg'},
+			 		 {url: '26.jpg'}, {url: '27.jpg'}, {url: '28.jpg'},
+			 		 {url: '29.jpg'} ],
+			 link: 'slimsteMensVan2014/galerij/galerij2/Ice_Bucket_challenge_Top10.mp4' },
+			{baseUrl:'slimsteMensVan2014/galerij/galerij3/',
+			 fotos: [{url: '30.jpg'}, {url: '31.jpg'}, {url: '32.jpg'},
+			 		 {url: '33.jpg'}, {url: '34.jpg'}, {url: '35.jpg'},
+			 		 {url: '36.jpg'}, {url: '37.jpg'}, {url: '38.jpg'},
+			 		 {url: '39.jpg'} ] },
+			],
+	collectiefGeheugen: {
+		videos: [
+			{urlVideo: 'slimsteMensVan2014/collectiefgeheugen/Collectief_Geheugen_1.mp4', antwoorden: ['Belgian Bullets', 'Elfje Willemsen', 'Hanna Mariën', '6de plaats', 'Sotsji'] },
+			{urlVideo: 'slimsteMensVan2014/collectiefgeheugen/Collectief_Geheugen_2.mp4', antwoorden: ['Oscar-uitreiking', 'Ellen De Generes', 'Group-selfie', 'Twitter-record', 'Meryl Streep'] },
+			{urlVideo: 'slimsteMensVan2014/collectiefgeheugen/Collectief_Geheugen_3.mp4', antwoorden: ['Cordon', 'Antwerpen', 'Dodelijk virus', 'Tom Dewispelaere', 'Sven De Ridder'] }
+		],
+	},
+	finale: [
+		{ vraag: 'Wie zijn de laatste 5 winnaars van de quiz \'De slimste mens ter wereld\'?',
+		  antwoorden: ['Freek Braeckman', 'Linda De Win', 'Tomas Van Den Spiegel', 'Gilles De Coster', 'Adil El Arbi'] },
+		{ vraag: 'Wat zijn de 5 belangrijkste goede voornemens?',
+		  antwoorden: ['Afvallen', 'Minder druk maken', 'Meer sporten / meer bewegen', 'Zuiniger met geld omgaan', 'Beter voor mezelf opkomen / vaker nee zeggen'] },
+		{ vraag: 'Wat weet je over Nafi(ssatou) Thiam?',
+		  antwoorden: ['Sportvrouw van het jaar', '7-kamp (hepthatlon)', 'Hoogspringen', 'Belgische recordhoudster', 'Senegalese roots'] },
+		{ vraag: 'Welke waren volgens Febiac de 5 best verkochte automerken in België?',
+		  antwoorden: ['Volkswagen', 'Renault', 'Peugeot', 'Citroën', 'Opel'] },
+		{ vraag: 'Wat weet je over Malala Yousafzai?',
+		  antwoorden: ['Nobelprijs Vrede', 'Kogel in het hoofd','Pakistan','Groot-Brittanië','Jongste winnaar ooit (17 jaar)',] },
+		{ vraag: 'Wat zijn de 5 grootste tijdverspillers op kantoor?',
+		  antwoorden: ['Perfectionisme','Rework (fouten herstellen)','Collegaâ€™s en onverwachte bezoekers','Noodgevallen / paniek!','E-mails'] },
+		{ vraag: 'Wat waren de juiste antwoorden op de eerste 5 vragen van deze quiz?',
+		  antwoorden: ['2014','Stedelijk Museum voor Actuele Kunst','24','Gangnam Style','1 - 7'] },
+		{ vraag: 'Wat weet je over James Foley?',
+		  antwoorden: ['Amerikaan','Persfotograaf','Ontvoerd','Onthoofd','IS'] },
+		{ vraag: 'Welk zijn de 5 bestbetaalde sporten?',
+		  antwoorden: ['Boksen', 'Voetbal', 'Basketball', 'Golf', 'Tennis'] },
+		{ vraag: 'Wat weet je over Nathalie Meskens?',
+		  antwoorden: ['Actrice','Jeroen Van Dyck','Yam Thai','Beste kijkers','Tegen de sterren op'] },
+		{ vraag: 'Welke zijn volgens de NMBS de drukste stations van België?',
+		  antwoorden: ['Brussel-Zuid','Brussel-Centraal','Gent Sint-Pieters','Brussel-Noord','Leuven'] },
+		{ vraag: 'Wat weet je over Dansdate?',
+		  antwoorden: ['Niels Destadsbader','Francesca Vanthielen','Dan Karaty','BVs dansen met eigen partner','Erik Goossens en Bieke Ilegems'] },
+		{ vraag: 'Welk zijn, volgens google de 5 meest gezochte Belgen van het afgelopen jaar?',
+		  antwoorden: ['Stromae','Dries Mertens','Luc De Vos','Axelle Despiegelaere','Thorgan Hazard'] },
+		{ vraag: 'Wat weet je over Conchita WÃ¼rst?',
+		  antwoorden: ['Vrouw met baard','Oostenrijk','Eurovisiesongfestival','Rise like a Phoenix','Kopenhagen'] },
+		{ vraag: 'Welke zijn de vijf luiste dieren ter wereld ?',
+		  antwoorden: ['Koala','Python','Vleermuis','Tijger','Hamster'] },
+		{ vraag: 'Wat weet je over Robin Williams?',
+		  antwoorden: ['Acteur','Depressief','Good morning Vietnam','Dead poets society','Oscar'] },
+		{ vraag: 'Welke zijn de 5 Europese landen waar het meeste gependeld wordt?',
+		  antwoorden: ['België','Italië','Spanje','Griekenland','Oostenrijk'] },
+		{ vraag: 'Geef de vijf belangrijkste redenen waarom we te laat komen',
+		  antwoorden: ['Ik raakte niet uit bed','Voor ik kon vertrekken moest ik nog iets afwerken','Ik had de drukte onderweg onderschat','Ik vond mijn gsm/sleutels/portefeuille niet','het gaat er thuis vaak chaotisch aan toe'] },
+		{ vraag: 'Wat weet je over Jean-Luc Dehaene?',
+		  antwoorden: ['Celie','Eerste minister','â€˜de loodgieterâ€™','Club Brugge','Vilvoorde'] },
+		{ vraag: 'Wat weet je over Vlucht MH17?',
+		  antwoorden: ['neergeschoten','Oost-Oekraïne (Hrabove)','Malaysia Airlines','Van Schiphol (Amsterdam)','Naar Kuala Lumpur'] },
+		{ vraag: 'Wat weet je over Koningin Fabiola?',
+		  antwoorden: ['Koning Boudewijn','Spaanse','Koningin Elisabeth-wedstrijd','De Indische waterlelies','Fabiola de Mora y Aragon'] },
+		{ vraag: 'Wat waren de 5 landen met de meeste medailles op de voorbije Olympische winterspelen?',
+		  antwoorden: ['Rusland','Verenigde staten','Noorwegen','Canada','Nederland'] },
+		{ vraag: 'Wat weet je over Luc Alloo?',
+		  antwoorden: ['Sandy Blanckaert','Paul Jambers','Sterren en kometen','Alloo in de gevangenis','De wegpolitie'] },
+		{ vraag: 'Wat weet je over \â€˜The Ice Bucket Challenge\â€™?',
+		  antwoorden: ['ALS','Geldinzameling','Emmer ijskoud water','Viraal','Video posten'] },
+		{ vraag: 'Noem alle tegenstanders van België op het afgelopen WK in Brazilië.',
+		  antwoorden: ['Algerije','Rusland','Zuid-Korea','Verenigde Staten','Argentinië'] },
+		{ vraag: 'Wat weet je over Stromae?',
+		  antwoorden: ['zanger','Paul Van Haver','Ta fÃªte','Eigen kleding-label','Opvallend strikje'] },
+		{ vraag: 'Wat weet je over Volkert Van der Graaf?',
+		  antwoorden: ['Nederlander','Moord op Pim Fortuyn','2002','Voorwaardelijk vrij','Obsessief-compulsief gedrag '] },
+		{ vraag: 'Welke waren de 5 favoriete vakantielanden van de Belgen voor lange vakanties?',
+		  antwoorden: ['Frankrijk','België','Spanje','Italië','Duitsland'] },
+		{ vraag: 'Wat weet je over Marc Wilmots?',
+		  antwoorden: ['Ex-voetballer','Nationale ploeg','Das kampfschwein','Coach van het jaar','Schalke 04'] },
+		{ vraag: 'Wat weet je over de pontonbrug in Antwerpen?',
+		  antwoorden: ['Herdenking WO I','Chaos','Schelde','Tijdelijke brug','Vernietigd'] },
+		{ vraag: 'Wat weet je over Divock Origi?',
+		  antwoorden: ['Lille','Liverpool','spits','Mike Origi','Rode Duivel'] },
+		{ vraag: 'Wat weet je over Klas Ingesson?',
+		  antwoorden: ['voetballer','KV Mechelen','kanker','Zweed','Nationale ploeg'] }
+	]
+};
